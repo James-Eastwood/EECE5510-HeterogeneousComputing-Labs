a2d937669d6dcff52ebef6c8e1a8e8389492ac70
James Eastwood <eastwood.james@icloud.com>
2023-06-02 17:33:00 -0400

    Update shell script for the matrix_multi to resolve issues related to fpga running

--- Labs/lab1/matrix_multi/fpga_matrix_multi.sh
+++ Labs/lab1/matrix_multi/fpga_matrix_multi.sh
@@ -1,3 +1,19 @@
+cd ~/EECE5510-HeterogeneousComputing-Labs/Labs/lab1/matrix_multi
+
+printf "\\n%s\\n" "================================================================================"
+printf "\\n%s\\n" "Running in Emulation Mode"
+printf "\\n%s\\n" "================================================================================"
+
+aoc -march=emulator -v device/matrix_multi.cl -o bin/matrix_multi_emulation.aocx
+ln -sf matrix_multi_emulation.aocx bin/matrix_multi.aocx
+make
+
+./bin/host -emulator
+
+printf "\\n%s\\n" "================================================================================"
+printf "\\n%s\\n" "Running in FPGA Mode"
+printf "\\n%s\\n" "================================================================================"
+
 # Initial Setup
 source /data/intel_fpga/devcloudLoginToolSetup.sh
 tools_setup -t A10DS
@@ -7,18 +23,26 @@ aocl diagnose
 error_check
 
 # Compiling OpenCL code for FPGA
-# cd ~/EECE5510-HeterogeneousComputing-Labs/Labs/lab1/matrix_multi/fpga_matrix_multi.sh
 aoc device/matrix_multi.cl -o bin/matrix_multi_fpga.aocx -board=pac_a10
 
-# 3.4 Converting the 1.2.1 version of .aocx to an unsigned .aocx file
+# Relink to hardware .aocx
+ln -sf bin/matrix_multi_fpga.aocx bin/matrix_multi.aocx
+# Availavility of Acceleration cards
+aoc -list-boards
+error_check
+# Get device name
+aocl diagnose
+error_check
+
+# Converting to an unsigned .aocx file
 cd bin
-yes | source $AOCL_BOARD_PACKAGE_ROOT/linux64/libexec/sign_aocx.sh -H openssl_manager -i matrix_multi_fpga.aocx -r NULL -k NULL -o matrix_multi_fpga_unsigned.aocx
+printf "\\n%s\\n" "Converting to unsigned .aocx:"
+printf "Y\\nY\\n" | source $AOCL_BOARD_PACKAGE_ROOT/linux64/libexec/sign_aocx.sh -H openssl_manager -i matrix_multi_fpga.aocx -r NULL -k NULL -o matrix_multi_fpga_unsigned.aocx
 error_check
 
 # 3.6 Programming the Arria 10 GX PAC Card
 aocl program acl0 matrix_multi_fpga_unsigned.aocx
 
 # 3.7 Running the host code
-make
 ./host
 error_check
